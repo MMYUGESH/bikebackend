5fe13d035989d9d4eff8439ba6584c5f432459c7
MMYUGESH <mmyugesh@gmail.com>
2021-05-05 18:05:39 +0530

    fourth commit

--- bike.js
+++ bike.js
@@ -12,7 +12,6 @@ app.use(cors());
 
 app.use(express.json());
 app.use(function(req, res, next) {  
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Headers","*");
 res.header('Access-Control-Allow-Credentials', true);
